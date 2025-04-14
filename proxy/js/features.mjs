4ca6edd4617899d020da08e96b4aa5bf2fd1fcea
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-04-14 20:56:31 +0200

    Fix signals not showing feature details and note/description fields (#344)
    From
    https://github.com/hiddewie/OpenRailwayMap-vector/pull/311#issuecomment-2770691179
    
    Followup of https://github.com/hiddewie/OpenRailwayMap-vector/pull/311
    
    Signals are not showing feature properties: Wikidata, Mapillary, images,
    Wikimedia Commons, Wikipedia, note & descriptions. The data is imported,
    and it should be shown in the map popups.
    
    
    https://openrailwaymap.fly.dev/#view=17.5/46.389245/-70.950886&style=signals
    Before:
    
    ![image](https://github.com/user-attachments/assets/51568b82-b4c4-4759-9370-f11970c88daf)
    
    After:
    
    
    ![image](https://github.com/user-attachments/assets/23aeb436-3821-4e2b-b37c-59625f8c8554)

--- proxy/js/features.mjs
+++ proxy/js/features.mjs
@@ -652,6 +652,34 @@ const features = {
       direction_both: {
         name: 'both directions',
       },
+      wikidata: {
+        name: 'Wikidata',
+        link: links.wikidata,
+      },
+      wikimedia_commons: {
+        name: 'Wikimedia',
+        link: links.wikimedia_commons,
+      },
+      image: {
+        name: 'Image',
+        link: links.image,
+      },
+      mapillary: {
+        name: 'Mapillary',
+        link: links.mapillary,
+      },
+      wikipedia: {
+        name: 'Wikipedia',
+        link: links.wikipedia,
+      },
+      note: {
+        name: 'Note',
+        paragraph: true,
+      },
+      description: {
+        name: 'Description',
+        paragraph: true,
+      },
     },
   },
   'openrailwaymap_signals-signals_signal_boxes': {
